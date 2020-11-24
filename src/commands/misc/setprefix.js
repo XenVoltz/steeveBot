6562e358bc2bc2253f4131dd95296cbba23042be
Victor <victorlobejr@hotmail.com>
2020-11-24 12:57:47 -0800

    [refactor] (setprefix) made it create new channel instead of renaming channel

--- src/commands/misc/setprefix.js
+++ src/commands/misc/setprefix.js
@@ -13,13 +13,13 @@ module.exports = {
 
         if (message.channel.type === 'dm') return;
         if (!message.guild.me.hasPermission('MANAGE_CHANNELS')) return message.channel.send("This command requires me to have \`MANAGE_CHANNELS\`");
-        
+
         const newPrefixChannel = message.guild.channels.cache.find(ch => ch.name.startsWith(`steeve-`))
         const args1 = args[0]
         if (args1 === set.prefix) return message.channel.send("That is already the prefix.")
         const newPrefix = args[0];
 
-            if (!newPrefixChannel) {
+        if (!newPrefixChannel) {
             const defaultChannelName = `steeve-${args1}`
             const channel = await message.guild.channels.create(defaultChannelName, {
                 type: 'voice',
@@ -27,14 +27,23 @@ module.exports = {
                     deny: 'VIEW_CHANNEL',
                     id: message.guild.id
                 }]
-    
+
             });
-            
+
             return message.channel.send(`My new prefix is \`${newPrefix}\`.`);
         }
-        
+
         const channel = await message.guild.channels.cache.find(ch => ch.name.startsWith("steeve-"));
-        await channel.setName(`steeve-${newPrefix}`);
+        await channel.delete();
+        const newChannel = `steeve-${args1}`
+        await message.guild.channels.create(newChannel, {
+            type: 'voice',
+            permissionOverwrites: [{
+                deny: 'VIEW_CHANNEL',
+                id: message.guild.id
+            }]
+
+        });
         message.channel.send(`My new prefix is \`${newPrefix}\`.`);
     }
 }
